963adcfc95ee0db50540c44bbb40c6b6ebed4ae5
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-12-29 19:35:34 -0300

    listeners and groups

--- str-consumer/src/main/java/github/guisofiati/strconsumer/listeners/StringConsumerListener.java
+++ str-consumer/src/main/java/github/guisofiati/strconsumer/listeners/StringConsumerListener.java
@@ -8,8 +8,19 @@ import org.springframework.stereotype.Component;
 @Log4j2
 public class StringConsumerListener {
 
+    //we've two partitions, therefore listener 'create' will be registered in one partition and 'log' listener in other
+    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void create(String message) {
+        log.info("CREATE ::: Received message: {}", message);
+    }
+
     @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-        log.info("Received message: {}", message);
+    public void log(String message) {
+        log.info("LOG ::: Received message: {}", message);
+    }
+
+    @KafkaListener(groupId = "group-3", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void history(String message) {
+        log.info("HISTORY ::: Received message: {}", message);
     }
 }
