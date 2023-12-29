4bf6abbdf3dcba490c6dff4fb10f9856710ceb71
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-12-29 09:40:32 -0300

    producer started

---
+++ str-producer/src/main/java/github/guisofiati/strproducer/StrProducerApplication.java
@@ -0,0 +1,13 @@
+package github.guisofiati.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrProducerApplication.class, args);
+	}
+
+}
