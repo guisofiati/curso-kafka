a2a60fa833f9b2d5efcc8df56c6066c98b146e54
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-12-29 19:57:09 -0300

    custom listener

--- str-consumer/src/main/java/github/guisofiati/strconsumer/listeners/StringConsumerListener.java
+++ str-consumer/src/main/java/github/guisofiati/strconsumer/listeners/StringConsumerListener.java
@@ -1,7 +1,7 @@
 package github.guisofiati.strconsumer.listeners;
 
+import github.guisofiati.strconsumer.custom.StringConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,17 +9,22 @@ import org.springframework.stereotype.Component;
 public class StringConsumerListener {
 
     //we've two partitions, therefore listener 'create' will be registered in one partition and 'log' listener in other
-    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+//    @KafkaListener(groupId = "group-1",
+//            topicPartitions = {
+//            @TopicPartition(topic = "str-topic", partitions = {"0"})
+//            },
+//            containerFactory = "strContainerFactory")
+    @StringConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
         log.info("CREATE ::: Received message: {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StringConsumerCustomListener(groupId = "group-1")
     public void log(String message) {
         log.info("LOG ::: Received message: {}", message);
     }
 
-    @KafkaListener(groupId = "group-3", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StringConsumerCustomListener(groupId = "group-2")
     public void history(String message) {
         log.info("HISTORY ::: Received message: {}", message);
     }
