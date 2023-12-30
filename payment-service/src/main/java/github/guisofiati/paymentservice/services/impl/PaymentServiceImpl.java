bf81025bc3fa1a334f0483c3793fa46ad52887ac
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-12-30 10:11:38 -0300

    send json to topic

--- payment-service/src/main/java/github/guisofiati/paymentservice/services/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/github/guisofiati/paymentservice/services/impl/PaymentServiceImpl.java
@@ -2,15 +2,27 @@ package github.guisofiati.paymentservice.services.impl;
 
 import github.guisofiati.paymentservice.models.PaymentModel;
 import github.guisofiati.paymentservice.services.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
     @Override
+    @SneakyThrows
     public void sendPayment(PaymentModel paymentModel) {
-        log.info("PAYMENT SERVICE IMPL ::: Payment received {}", paymentModel);
+        log.info("Payment received: {}", paymentModel);
+        Thread.sleep(1000);
+        log.info("Sending payment...");
+        kafkaTemplate.send("payment-topic", paymentModel);
     }
 }
